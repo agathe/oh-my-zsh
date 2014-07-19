41e337dc13180885701a778de6c8e7066f6c3603
Agathe Battestini <agathe.battestini@gmail.com>
2014-07-19 08:52:32 -0700

    new aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -34,3 +34,14 @@ alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
 
+# editors
+alias stt='subl'
+alias edit='subl'
+alias opent='open -a "Sublime Text"'
+alias openr='open -a "RubyMine"'
+alias openx='open -a "XCode"'
+alias opena='open -a "AppCode"'
+alias openc='open_text_in_chrome'
+alias zcheat='open http://ivan.kanis.fr/zsh.pdf'
+alias whatison80='sudo lsof -i :80'
+
